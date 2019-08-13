25cf13859a24a0a69bb11cc6d6aff28e8eaf0056
Alexander Kostyurenko <alekkostyur@gmail.com>
2019-08-13 23:06:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getColor(R.color.red);
+        }
+
+        int red = ContextCompat.getColor(this, R.color.red);
+
+        final Button button = findViewById(R.id.click_button);
+        button.setBackgroundColor(red);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button.setText(getString(R.string.new_text));
+            }
+        });
+    }
+}
